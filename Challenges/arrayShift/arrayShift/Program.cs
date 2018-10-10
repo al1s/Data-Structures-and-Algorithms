03d744eeabf1173ad50bdff1b5752ed822911294
alstof <alstof@gmail.com>
2018-10-09 21:43:22 -0700

    Add readme

---
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace arrayShift
+{
+    class Program
+    {
+        static int[] ArrayShift(int[] array, int number)
+        {
+            return default(int[]);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ArrayShift(new int[] { 2, 4, 6, 7 }, 5);
+            ArrayShift(new int[] { 4, 8, 15, 23, 42 }, 16);
+        }
+    }
+}
