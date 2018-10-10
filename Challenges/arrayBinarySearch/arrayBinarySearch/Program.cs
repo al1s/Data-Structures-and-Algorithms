6f50a6e4a92ea31ae1ddccaf82aaa7261b475cb7
alstof <alstof@gmail.com>
2018-10-10 15:13:32 -0700

    Initial setup

---
+++ Challenges/arrayBinarySearch/arrayBinarySearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrayBinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
