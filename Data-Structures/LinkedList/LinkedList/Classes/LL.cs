d65f06db18f36ffe9d0658649e0fb97bc1cf5339
alstof <alstof@gmail.com>
2018-10-16 13:05:06 -0700

    Add basic functionality

---
+++ Data-Structures/LinkedList/LinkedList/Classes/LL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    class LL
+    {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+        public LL(Node newNode)
+        {
+            Head = newNode;
+            Current = newNode;
+        }
+        /// <summary>
+        /// Add new element with a value to the linked list
+        /// </summary>
+        /// <param name="value">Value to be added</param>
+        public void Add(object value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Head;
+            Head = newNode;
+            Current = newNode;
+        }
+        /// <summary>
+        /// Print to console the content of the linked list
+        /// </summary>
+        public void Print()
+        {
+            Current = Head;
+            while (Current.Next != null)
+            {
+                Console.WriteLine($"{Current.Value} -->");
+                Current = Current.Next;
+            } 
+            Console.WriteLine($"{Current.Value} --> null");
+            Current = Head;
+
+        }
+    }
+}
---
+++ Data-Structures/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node(10);
+            LL ll = new LL(node);
+            ll.Add(3);
+            ll.Add(6);
+            ll.Print();
+            Console.ReadLine();
+        }
+    }
+}
