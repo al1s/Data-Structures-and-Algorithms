3885c53f6909133d4c895add667c46eb172f3599
alstof <alstof@gmail.com>
2018-10-17 00:05:48 -0700

    Initial comment

---
+++ Challenges/llKthFromEnd/llKthFromEnd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace llKthFromEnd
+{
+
+    class Program
+    {
+        public static int GetKthFromEnd(LL ll, int number)
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
