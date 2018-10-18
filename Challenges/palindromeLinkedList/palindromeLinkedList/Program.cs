a76a317f640ee2ea1c8c99cc48a348f75d455696
alstof <alstof@gmail.com>
2018-10-18 14:36:25 -0700

    Initial commit

---
+++ Challenges/palindromeLinkedList/palindromeLinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace palindromeLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/palindromeLinkedList/palindromeLinkedListTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using LinkedList.Classes;
+using palindromeLinkedList;
+
+namespace palindromeLinkedListTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
