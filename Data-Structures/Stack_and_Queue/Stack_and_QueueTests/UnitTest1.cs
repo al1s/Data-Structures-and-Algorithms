af5d8ee4b15654e9146167557971346e6b6f7385
alstof <alstof@gmail.com>
2018-10-22 18:03:47 -0700

    Add tests skeleton

--- Data-Structures/Stack_and_Queue/Stack_and_QueueTests/UnitTest1.cs
+++ Data-Structures/Stack_and_Queue/Stack_and_QueueTests/UnitTest1.cs
@@ -6,7 +6,32 @@ namespace Stack_and_QueueTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanPushNodeToStack()
+        {
+
+        }
+        [Fact]
+        public void CanPopNodeFromStack()
+        {
+
+        }
+        [Fact]
+        public void CanPeekNodeFromStack()
+        {
+
+        }
+        [Fact]
+        public void CanEnqueueNodeToQueue()
+        {
+
+        }
+        [Fact]
+        public void CanDequeueNodeFromQueue()
+        {
+
+        }
+        [Fact]
+        public void CanPeekNodeToQueue()
         {
 
         }
