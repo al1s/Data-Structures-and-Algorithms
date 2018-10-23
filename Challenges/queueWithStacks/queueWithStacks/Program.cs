f39406f156b2b76ca343e3254e5fcc8bc546b007
alstof <alstof@gmail.com>
2018-10-23 01:50:20 -0700

    Add queueu with 2 stacks implementation

---
+++ Challenges/queueWithStacks/queueWithStacks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stack_and_Queue.Classes;
+
+namespace queueWithStacks
+{
+    class Program
+    {
+        public void QueueWithStacks()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/queueWithStacks/queueWithStacksTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using Stack_and_Queue.Classes;
+using queueWithStacks.Classes;
+
+namespace queueWithStacksTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanEnqueue()
+        {
+
+        }
+        public void CanDequeue()
+        {
+
+        }
+    }
+}
