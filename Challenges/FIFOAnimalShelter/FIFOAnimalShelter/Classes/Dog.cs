fac2cda32fd60e6fdac1930bc73792588e048c31
alstof <alstof@gmail.com>
2018-10-24 05:59:57 -0700

    Add comments

---
+++ Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Dog.cs
@@ -0,0 +1,12 @@
+﻿namespace FIFOAnimalShelter.Classes
+{
+    public class Dog : Animal
+    {
+        public override string Name { get; set; }
+        public override Animal Next { get; set; }
+        public Dog(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Snake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    class Snake : Animal
+    {
+        public override string Name { get; set; }
+        public override Animal Next { get; set; }
+        public Snake(string name)
+        {
+            Name = name;
+        }
+    }
+}
