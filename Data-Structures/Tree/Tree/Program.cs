6f61effff41de3acdb72f99358b06fb4d67c9342
alstof <alstof@gmail.com>
2018-10-29 11:42:12 -0700

    Add binary tree implementation

--- Data-Structures/Tree/Tree/Program.cs
+++ Data-Structures/Tree/Tree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Tree.Classes;
+using System.Collections.Generic;
 
 namespace Tree
 {
@@ -7,7 +8,28 @@ namespace Tree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            BinaryTree<int> bT = new BinaryTree<int>();
+            bT.Add(0);
+            bT.Add(1);
+            bT.Add(2);
+            bT.Add(3);
+            bT.Add(4);
+            bT.Add(5);
+            bT.Add(6);
+            Console.WriteLine("Preordered tree:");
+            bT.PreOrder(bT.Root);
+            Array.ForEach<Node<int>>(bT.arrayResult.ToArray(), elm => Console.Write($"{elm.Value}, "));
+            bT.arrayResult = new List<Node<int>>();
+            Console.WriteLine();
+            Console.WriteLine("Inordered tree:");
+            bT.InOrder(bT.Root);
+            Array.ForEach<Node<int>>(bT.arrayResult.ToArray(), elm => Console.Write($"{elm.Value}, "));
+            bT.arrayResult = new List<Node<int>>();
+            Console.WriteLine();
+            Console.WriteLine("Postordered tree:");
+            bT.PostOrder(bT.Root);
+            Array.ForEach<Node<int>>(bT.arrayResult.ToArray(), elm => Console.Write($"{elm.Value}, "));
+            Console.ReadLine();
+        } 
     }
 }
