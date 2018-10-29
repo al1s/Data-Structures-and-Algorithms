6555f047eed635622894527481c75fdc41c8d731
alstof <alstof@gmail.com>
2018-10-29 07:44:56 -0700

    Add skeleton for tree structure

---
+++ Data-Structures/Tree/TreeTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Tree;
+using Tree.Classes;
+
+namespace TreeTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
