b0a05d8a9c012b128d5344792606fe2178e05bc5
alstof <alstof@gmail.com>
2018-10-29 11:42:23 -0700

    Add addition to BST

---
+++ Data-Structures/Tree/Tree/Classes/BinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    public class BinarySearchTree<T>
+    {
+        public BinaryTree<int> _bt { get; set; }
+        public BinarySearchTree()
+        {
+            _bt = new BinaryTree<int>();
+        }
+        public void Add(int value)
+        {
+            this.Add(value, _bt.Root);
+        }
+        public void Add(int value, Node<int> root)
+        {
+            if(root == null)
+            {
+                root = new Node<int>(value);
+            }
+            if(value > root.Value)
+            {
+                this.Add(value, root.RightChild);
+            }
+            else if(value < root.Value)
+            {
+                this.Add(value, root.LeftChild);
+            }
+        }
+        public Node<T> Search(T value)
+        {
+
+            return default(Node<T>);
+        }
+    }
+}
