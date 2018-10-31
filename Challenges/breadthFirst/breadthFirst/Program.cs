41af1c0fa2e67b919ab4a629573fc9080ca4af68
alstof <alstof@gmail.com>
2018-10-31 14:36:38 -0700

    Add a skeleton for breadth-width traversal of tree

---
+++ Challenges/breadthFirst/breadthFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace breadthFirst
+{
+    public class Program
+    {
+        public static List<Node<int>> BreadthFirst(BinaryTree<int> tree)
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/breadthFirst/breadthFirstTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace breadthFirstTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
