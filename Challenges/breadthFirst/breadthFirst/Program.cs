4f2fd9205735c32882ad11bd60f05452cfcf799f
alstof <alstof@gmail.com>
2018-10-31 14:56:39 -0700

    Add implementation of BF traversal

--- Challenges/breadthFirst/breadthFirst/Program.cs
+++ Challenges/breadthFirst/breadthFirst/Program.cs
@@ -6,13 +6,38 @@ namespace breadthFirst
 {
     public class Program
     {
-        public static List<Node<int>> BreadthFirst(BinaryTree<int> tree)
+        /// <summary>
+        /// Given a tree return a list of nodes in breadth-first traversal order 
+        /// </summary>
+        /// <param name="tree">A tree to traverse</param>
+        /// <returns>A list of nodes</returns>
+        public static List<int> BreadthFirst(BinaryTree<int> tree)
         {
-
+            List<int> result = new List<int>();
+            Queue<Node<int>> queue = new Queue<Node<int>>();
+            queue.Enqueue(tree.Root);
+            while(queue.Count > 0)
+            {
+                Node<int> node = queue.Dequeue();
+                if(node.LeftChild != null)
+                    queue.Enqueue(node.LeftChild);
+                if(node.RightChild != null)
+                    queue.Enqueue(node.RightChild);
+                result.Add(node.Value);
+            }
+            return result;
         }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryTree<int> bt = new BinaryTree<int>();
+            bt.Add(2);
+            bt.Add(9);
+            bt.Add(5);
+            bt.Add(7);
+            bt.Add(32);
+            bt.Add(29);
+            BreadthFirst(bt).ForEach(elm => Console.Write($"{elm}, "));
+            Console.ReadLine();
         }
     }
 }
