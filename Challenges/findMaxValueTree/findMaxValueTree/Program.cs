7c1f150a90611501241ac549178ede8615247da6
alstof <alstof@gmail.com>
2018-11-04 15:13:46 -0800

    Add demo

--- Challenges/findMaxValueTree/findMaxValueTree/Program.cs
+++ Challenges/findMaxValueTree/findMaxValueTree/Program.cs
@@ -26,7 +26,23 @@ namespace findMaxValueTree
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryTree<int> tree = new BinaryTree<int>();
+            tree.Add(10);
+            tree.Add(1);
+            tree.Add(20);
+            tree.Add(31);
+            tree.Add(15);
+            tree.Add(45);
+            tree.Add(85);
+            tree.PreOrder(tree.Root);
+            Console.Write("The tree elements are: ");
+            Array.ForEach(tree.ToArray(), 
+                elm => Console.Write($"{elm}, ")
+            );
+            Console.WriteLine();
+            Console.Write("Max value of the tree: ");
+            Console.Write(FindMaxValue(tree).ToString());
+            Console.ReadLine();
         }
     }
 }
