e48c7301561f370816a00d48df51492159212ddd
alstof <alstof@gmail.com>
2018-11-04 15:08:31 -0800

    Add tests

--- Challenges/findMaxValueTree/findMaxValueTree/Program.cs
+++ Challenges/findMaxValueTree/findMaxValueTree/Program.cs
@@ -5,9 +5,24 @@ namespace findMaxValueTree
 {
     public class Program
     {
+        /// <summary>
+        /// Given a binary tree of integers returns the max int value
+        /// </summary>
+        /// <param name="tree">Binary tree to search through</param>
+        /// <returns>Max integer value</returns>
         public static int FindMaxValue(BinaryTree<int> tree)
         {
-            
+            int maxValue = int.MinValue;     
+            void TreeTraverser(Node<int> root)
+            {
+                if (root.Value > maxValue) maxValue = root.Value;
+                if (root.LeftChild != null)
+                    TreeTraverser(root.LeftChild);
+                if (root.RightChild != null)
+                    TreeTraverser(root.RightChild);
+            }
+            TreeTraverser(tree.Root);
+            return maxValue;
         }
         static void Main(string[] args)
         {
--- Challenges/findMaxValueTree/findMaxValueTreeTests/UnitTest1.cs
+++ Challenges/findMaxValueTree/findMaxValueTreeTests/UnitTest1.cs
@@ -2,13 +2,28 @@ using System;
 using Xunit;
 using Tree.Classes;
 using findMaxValueTree;
+using System.Diagnostics;
+using System.Linq;
 
 namespace findMaxValueTreeTests
 {
     public class FindMaxTreeTests
     {
+        /// <summary>
+        /// Can return an integer given a binary tree
+        /// </summary>
         [Fact]
-        public void GiveTree_GetMaxValue()
+        public void FindMaxValue_GivenATree_ReturnsAnInt()
+        {
+            BinaryTree<int> tree = new BinaryTree<int>();
+            tree.Add(1);
+            Assert.IsType<int>(Program.FindMaxValue(tree));
+        }
+        /// <summary>
+        /// Can return max int value in a binary tree
+        /// </summary>
+        [Fact]
+        public void FindMaxValue_GivenATree_ReturnsMaxValue()
         {
             int maxValue = 85;
             BinaryTree<int> tree = new BinaryTree<int>();
@@ -18,6 +33,17 @@ namespace findMaxValueTreeTests
             tree.Add(31);
             tree.Add(15);
             tree.Add(45);
+            tree.Add(maxValue);
+            Assert.Equal(maxValue, Program.FindMaxValue(tree));
+        }
+        /// <summary>
+        /// Can return max negative value of a binary tree
+        /// </summary>
+        [Fact]
+        public void FindMaxValue_GivenATreeOfOneElm_ReturnsTheElementValue()
+        {
+            int maxValue = -85;
+            BinaryTree<int> tree = new BinaryTree<int>();
             tree.Add(maxValue);
             Assert.Equal(maxValue, Program.FindMaxValue(tree));
         }
