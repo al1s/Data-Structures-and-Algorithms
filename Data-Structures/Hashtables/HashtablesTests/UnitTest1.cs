e703cb38d1cec2ab9d9fbc36089465140a07d648
alstof <alstof@gmail.com>
2018-11-13 15:16:19 -0800

    Add tests skeleton

--- Data-Structures/Hashtables/HashtablesTests/UnitTest1.cs
+++ Data-Structures/Hashtables/HashtablesTests/UnitTest1.cs
@@ -3,10 +3,61 @@ using Xunit;
 
 namespace HashtablesTests
 {
-    public class UnitTest1
+    public class HashtableMethods
     {
+        /// <summary>
+        /// Test whether we can add new element to a hashtable
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void AddElmToHashTable_OnAdd_ElementStored()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add new element with the same hash to a hashtable
+        /// </summary>
+        [Fact]
+        public void AddElmToHashTable_OnAddElmWithSameHash_ElementStored()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add new element with the same key to a hashtable
+        /// </summary>
+        [Fact]
+        public void AddElmToHashTable_OnAddElmWithSameKey_ExceptionThrown()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add find element with the key in a hashtable
+        /// </summary>
+        [Fact]
+        public void FindElmInHashTable_OnExistingKey_ReturnNodeForAKey()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add find element with the key in a hashtable
+        /// </summary>
+        [Fact]
+        public void FindElmInHashTable_OnNonExistingKey_ReturnNullNode()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add check existence of an element with the key in a hashtable
+        /// </summary>
+        [Fact]
+        public void ContainsElmInHashTable_OnExistingKey_ReturnTrue()
+        {
+
+        }
+        /// <summary>
+        /// Test whether we can add check existence of an element with the key in a hashtable
+        /// </summary>
+        [Fact]
+        public void ContainsElmInHashTable_OnNonExistingKey_ReturnFalse()
         {
 
         }
