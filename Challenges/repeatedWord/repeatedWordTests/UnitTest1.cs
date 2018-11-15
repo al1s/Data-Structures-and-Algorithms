37cbc929b4df4ca7dcea37deece4b3a9632d7915
alstof <alstof@gmail.com>
2018-11-14 16:37:54 -0800

    Add tests

--- Challenges/repeatedWord/repeatedWordTests/UnitTest1.cs
+++ Challenges/repeatedWord/repeatedWordTests/UnitTest1.cs
@@ -12,7 +12,12 @@ namespace repeatedWordTests
         [Fact]
         public void OnEmptyInput_ReturnsEmptyString()
         {
-
+            // arrange
+            string emptyText = string.Empty;
+            // act
+            string actualResult = Program.RepeatedWord(emptyText);
+            // assert
+            Assert.Equal(emptyText, actualResult);
         }
         /// <summary>
         /// Can return an empty string for an input text without repetitions 
@@ -20,7 +25,12 @@ namespace repeatedWordTests
         [Fact]
         public void OnInputWithoutRepeatedWords_ReturnsEmptyString()
         {
-
+            // arrange
+            string textWithoutRepetitions = "a";
+            // act
+            string actualResult = Program.RepeatedWord(textWithoutRepetitions);
+            // assert
+            Assert.Equal(string.Empty, actualResult);
         }
         /// <summary>
         /// Can return an repeated string for an input text with repetitions 
@@ -28,7 +38,13 @@ namespace repeatedWordTests
         [Fact]
         public void OnInputWithRepeatedWords_ReturnsRepeatedString()
         {
-
+            // arrange
+            string textWithRepetitions = "A fox is a fox";
+            string expectedResult = "a";
+            // act
+            string actualResult = Program.RepeatedWord(textWithRepetitions);
+            // assert
+            Assert.Equal(expectedResult, actualResult);
         }
     }
 }
