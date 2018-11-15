5cc040988c43842ce2dc6328c912ff821c2d27a5
alstof <alstof@gmail.com>
2018-11-14 16:26:18 -0800

    Add implementation

--- Challenges/repeatedWord/repeatedWord/Program.cs
+++ Challenges/repeatedWord/repeatedWord/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace repeatedWord
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Given a text return a first repeated word (case-insencitive)
+        /// </summary>
+        /// <param name="text">Text to check for repeated words</param>
+        /// <returns>The first word which was repeated</returns>
+        public static string RepeatedWord(string text)
         {
-            Console.WriteLine("Hello World!");
+            if (text.Length == 0) return string.Empty;
+            Dictionary<string, bool> dict = new Dictionary<string, bool>();
+            string[] textArr = text.ToLower().Split(" ");
+            foreach (string word in textArr)
+            {
+                if (dict.ContainsKey(word))
+                    return word;
+                dict.Add(word, true);
+            }
+            return string.Empty;
+        }
+        public static void Main(string[] args)
+        {
+            string text = "A fox is a fox";
+            Console.WriteLine($"{text} : {RepeatedWord(text)}");
+            text = "A";
+            Console.WriteLine($"{text} : {RepeatedWord(text)}");
+            text = "";
+            Console.WriteLine($"{text} : {RepeatedWord(text)}");
+            Console.ReadLine();
         }
     }
 }
