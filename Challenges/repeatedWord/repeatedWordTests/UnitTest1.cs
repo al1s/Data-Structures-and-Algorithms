9313d9adc0cbcc14e39349db25db880b10aab15a
alstof <alstof@gmail.com>
2018-11-14 16:31:40 -0800

    Add tests skeleton

--- Challenges/repeatedWord/repeatedWordTests/UnitTest1.cs
+++ Challenges/repeatedWord/repeatedWordTests/UnitTest1.cs
@@ -6,8 +6,27 @@ namespace repeatedWordTests
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Can return an empty string for empty an input text
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void OnEmptyInput_ReturnsEmptyString()
+        {
+
+        }
+        /// <summary>
+        /// Can return an empty string for an input text without repetitions 
+        /// </summary>
+        [Fact]
+        public void OnInputWithoutRepeatedWords_ReturnsEmptyString()
+        {
+
+        }
+        /// <summary>
+        /// Can return an repeated string for an input text with repetitions 
+        /// </summary>
+        [Fact]
+        public void OnInputWithRepeatedWords_ReturnsRepeatedString()
         {
 
         }
