725b1f110560684f84ed4c10dfb3a4f5c36e1fab
alstof <alstof@gmail.com>
2018-11-19 15:18:24 -0800

    Add implementation

---
+++ Challenges/insertionSort/insertionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace insertionSort
+{
+    
+    class Program
+    {
+        static void InsertionSort(int[] inpArr)
+        {
+            for (int i = 1; i < inpArr.Length; i++)
+            {
+                int temp = inpArr[i];
+                int j = i - 1;
+
+                while (j >= 0 && temp < inpArr[i])
+                {
+                    inpArr[j + 1] = inpArr[j];
+                    j--;
+                }
+                inpArr[j + 1] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
